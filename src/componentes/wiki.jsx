698c32ab94105f5d20574169c6406336b07018ff
jjamadeu <joaopedrosussa@gmail.com>
2023-12-15 00:15:39 -0300

    mudança de cor

--- src/componentes/wiki.jsx
+++ src/componentes/wiki.jsx
@@ -50,7 +50,7 @@ export default function Wiki(props) {
                             imgPais={pais}
                         />
                     </div>
-
+        
                 </div>
 
                 <div id="paragrafo">
